461c7a5126cce3ec614a1c3c450125d3eea35487
Bogo <bogdaneusebiu@gmail.com>
2026-02-15 23:35:53 +0200

    added unittests and ENUM

--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
-print("hello world")
+from textnode import *
+
+text = TextNode("This is some anchor text", TextType.IMAGE, "https://www.boot.dev")
+
+print(text)
---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text, text_type, image = None):
+        self.text = text
+        self.text_type = text_type
+        self.image = image
+    
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.image == other.image
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.image})"
